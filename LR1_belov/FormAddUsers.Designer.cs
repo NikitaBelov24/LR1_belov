0599b9221a996ea3ca7d271b23938eb4240fdc99
Белов Никита <is25belovnv@artcollege.ru>
2024-09-07 17:37:37 +0300

    Добавьте файлы проекта.

---
+++ LR1_belov/FormAddUsers.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace LR1_belov
+{
+    partial class FormAddUsers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.radioButtonMen = new System.Windows.Forms.RadioButton();
+            this.radioButtonWoman = new System.Windows.Forms.RadioButton();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox2 = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.first_NameTextBox = new System.Windows.Forms.TextBox();
+            this.second_NameTextBox = new System.Windows.Forms.TextBox();
+            this.roleIDComboBox = new System.Windows.Forms.ComboBox();
+            this.PhonemaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.BindingSourceRole = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.BindingSourceRole)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(231, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(353, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Добавить нового пользователя";
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonBack.Location = new System.Drawing.Point(33, 10);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(93, 33);
+            this.buttonBack.TabIndex = 1;
+            this.buttonBack.Text = "Назад";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAdd.Location = new System.Drawing.Point(645, 395);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(117, 33);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(33, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Логин:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(33, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Пароль:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(33, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Повтор пароля:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(33, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Эл. почта:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(33, 283);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Телефон:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(502, 121);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Фамилия:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(502, 160);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 16);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Имя:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(502, 203);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 16);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Роль:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(502, 242);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(36, 16);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Пол:";
+            // 
+            // radioButtonMen
+            // 
+            this.radioButtonMen.AutoSize = true;
+            this.radioButtonMen.Checked = true;
+            this.radioButtonMen.Location = new System.Drawing.Point(580, 242);
+            this.radioButtonMen.Name = "radioButtonMen";
+            this.radioButtonMen.Size = new System.Drawing.Size(71, 17);
+            this.radioButtonMen.TabIndex = 12;
+            this.radioButtonMen.TabStop = true;
+            this.radioButtonMen.Text = "Мужской";
+            this.radioButtonMen.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonWoman
+            // 
+            this.radioButtonWoman.AutoSize = true;
+            this.radioButtonWoman.Location = new System.Drawing.Point(691, 242);
+            this.radioButtonWoman.Name = "radioButtonWoman";
+            this.radioButtonWoman.Size = new System.Drawing.Size(72, 17);
+            this.radioButtonWoman.TabIndex = 13;
+            this.radioButtonWoman.Text = "Женский";
+            this.radioButtonWoman.UseVisualStyleBackColor = true;
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginTextBox.Location = new System.Drawing.Point(159, 121);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(199, 22);
+            this.loginTextBox.TabIndex = 14;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordTextBox.Location = new System.Drawing.Point(159, 157);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(199, 22);
+            this.passwordTextBox.TabIndex = 15;
+            // 
+            // passwordTextBox2
+            // 
+            this.passwordTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordTextBox2.Location = new System.Drawing.Point(159, 200);
+            this.passwordTextBox2.Name = "passwordTextBox2";
+            this.passwordTextBox2.PasswordChar = '*';
+            this.passwordTextBox2.Size = new System.Drawing.Size(199, 22);
+            this.passwordTextBox2.TabIndex = 16;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.emailTextBox.Location = new System.Drawing.Point(159, 242);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(199, 22);
+            this.emailTextBox.TabIndex = 17;
+            // 
+            // first_NameTextBox
+            // 
+            this.first_NameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.first_NameTextBox.Location = new System.Drawing.Point(577, 115);
+            this.first_NameTextBox.Name = "first_NameTextBox";
+            this.first_NameTextBox.Size = new System.Drawing.Size(199, 22);
+            this.first_NameTextBox.TabIndex = 19;
+            // 
+            // second_NameTextBox
+            // 
+            this.second_NameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.second_NameTextBox.Location = new System.Drawing.Point(577, 154);
+            this.second_NameTextBox.Name = "second_NameTextBox";
+            this.second_NameTextBox.Size = new System.Drawing.Size(199, 22);
+            this.second_NameTextBox.TabIndex = 20;
+            // 
+            // roleIDComboBox
+            // 
+            this.roleIDComboBox.DataSource = this.BindingSourceRole;
+            this.roleIDComboBox.DisplayMember = "Name";
+            this.roleIDComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.roleIDComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.roleIDComboBox.FormattingEnabled = true;
+            this.roleIDComboBox.Location = new System.Drawing.Point(577, 200);
+            this.roleIDComboBox.Name = "roleIDComboBox";
+            this.roleIDComboBox.Size = new System.Drawing.Size(199, 24);
+            this.roleIDComboBox.TabIndex = 21;
+            this.roleIDComboBox.ValueMember = "ID";
+            // 
+            // PhonemaskedTextBox
+            // 
+            this.PhonemaskedTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PhonemaskedTextBox.Location = new System.Drawing.Point(159, 283);
+            this.PhonemaskedTextBox.Mask = "+7 (000) 000-00-00";
+            this.PhonemaskedTextBox.Name = "PhonemaskedTextBox";
+            this.PhonemaskedTextBox.Size = new System.Drawing.Size(199, 22);
+            this.PhonemaskedTextBox.TabIndex = 22;
+            // 
+            // BindingSourceRole
+            // 
+            this.BindingSourceRole.DataSource = typeof(LR1_belov.DBContext.Roles);
+            // 
+            // FormAddUsers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 450);
+            this.Controls.Add(this.PhonemaskedTextBox);
+            this.Controls.Add(this.roleIDComboBox);
+            this.Controls.Add(this.second_NameTextBox);
+            this.Controls.Add(this.first_NameTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.passwordTextBox2);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginTextBox);
+            this.Controls.Add(this.radioButtonWoman);
+            this.Controls.Add(this.radioButtonMen);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "FormAddUsers";
+            this.Text = "FormAddUsers";
+            this.Load += new System.EventHandler(this.FormAddUsers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.BindingSourceRole)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.RadioButton radioButtonMen;
+        private System.Windows.Forms.RadioButton radioButtonWoman;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox2;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox first_NameTextBox;
+        private System.Windows.Forms.TextBox second_NameTextBox;
+        private System.Windows.Forms.ComboBox roleIDComboBox;
+        private System.Windows.Forms.MaskedTextBox PhonemaskedTextBox;
+        private System.Windows.Forms.BindingSource BindingSourceRole;
+    }
+}
---
+++ LR1_belov/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LR1_belov.DBContext;
+
+namespace LR1_belov
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        Model1 model = new Model1();
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = model.Users.ToList();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            FormAddUsers form = new FormAddUsers();
+            form.ShowDialog();
+            dataGridView1.DataSource = model.Users.ToList();
+        }
+    }
+}
