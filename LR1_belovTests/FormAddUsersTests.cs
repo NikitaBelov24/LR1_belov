ff72fa0b2837a33ce41cb8b826e0cfd06af1a2e3
Белов Никита <is25belovnv@artcollege.ru>
2024-10-22 22:04:17 +0300

    создан unit-test

---
+++ LR1_belovTests/FormAddUsersTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LR1_belov;
+using System;
+using Moq;
+
+namespace LR1_belov.Tests
+{
+    [TestClass()]
+    public class FormAddUsersTests
+    {
+        [TestMethod()]
+        public void ButtonBack_Click_CallsCloseMethod()
+        {
+            // Action
+            var form = new Mock<FormAddUsers>();
+            form.CallBase = true; // Нужно для того, чтобы вызывать базовый метод кнопки
+
+            // Assertion
+            form.Object.buttonBack_Click(this, EventArgs.Empty); // Вызов метода с передачей аргументов
+
+            // Arrangement
+            form.Verify(f => f.Close(), Times.Once); // Проверяем, что метод Close был вызван один раз
+        }
+    }
+}
